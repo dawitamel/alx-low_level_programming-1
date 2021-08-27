89f48858b65d9df9f5923de287a8770c2cf3ac9e
sofology <sofisintayehu19@gmail.com>
2021-08-27 14:43:09 +0300

    a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 /**
- * main - Prints out a string to stdout.
- * Return: 0 if success.
- */
+* main - Outputs byte size of native data types
+*
+* Return: (0) if output successful with no errors
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
