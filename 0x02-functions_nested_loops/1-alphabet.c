95b4cae86a2d5f29824e768588b99a0419bcac11
sofology <sofisintayehu19@gmail.com>
2021-08-31 21:47:56 +0300

    prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_alphabet - print the alphabet
- *
- * Description: print the alphabet, in lowercase, followed by a new line
- * Return: void
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
  */
-
 void print_alphabet(void)
 {
-	int c;
+	char c;
 
-	for (c = 'a'; c <= 'z'; ++c)
+	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
+	}
 	_putchar('\n');
 }
