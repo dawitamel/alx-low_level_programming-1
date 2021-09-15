53867e0727c1b5358b083c37b5c905de2dad04de
sofology <36291768+sofology@users.noreply.github.com>
2021-09-15 13:04:53 +0300

    a function that returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include"main.h"
+
+/**
+ * _power - Function that valued if is prime
+ * @a: Number to be valued
+ * @b: Parameter for value
+ * Return: Return the value
+ */
+
+int _power(int a, int b)
+{
+	if ((b * b) == a)
+	{
+		return (b);
+	}
+	else if ((b * b) < a)
+	{
+		return (_power(a, b + 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
+
+/**
+ * _sqrt_recursion - Function that returns the natural square root
+ * @n: Number to be valued
+ * Return: The natural square root or -1 if don't have square
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	return (_power(n, 2));
+}
