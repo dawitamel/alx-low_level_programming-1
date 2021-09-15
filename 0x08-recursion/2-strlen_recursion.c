e7c6d84900ddf120055c461add21de666ca52123
sofology <36291768+sofology@users.noreply.github.com>
2021-09-15 11:45:29 +0300

    a function that returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion - function that show the length of a string
+ * @s: the string
+ * Description: returns the length of a string
+ * Return: the lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+}
