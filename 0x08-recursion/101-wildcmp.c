fc96b4c19ad4019383553c29221b31d31e6cbb3a
sofology <36291768+sofology@users.noreply.github.com>
2021-09-15 16:34:21 +0300

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,3 +25,54 @@ int wildcmp(char *s1, char *s2)
 	else
 		return (0);
 }
+
+/**
+ *asterisks - iterates over asterisks and decides on activity
+ *@s1: first string
+ *@s2: second string
+ *@origin: keep track of starting pointer s1
+ *
+ *Return: 1 if the strings are the same, 0 if not
+ */
+int asterisks(char *s1, char *s2, char *origin)
+{
+
+	if (*s2 == '*') /*Iterate over series of '*' */
+	{
+		s2++;
+		return (asterisks(s1, s2, origin));
+	}
+	else if (*s2 == '\0') /*If you hit '\0'*/
+	{
+		if (*s1 == *origin)
+			return (1);
+		if (*s2 != *s1)
+			return (0);
+		else
+			return (1);
+	}
+	else /*If you hit another character*/
+	{
+		if (*s1 != *s2)
+		{
+			if (*s1 == '\0')
+			{
+				return (0);
+			}
+			else
+			{
+				s1++;
+				return (asterisks(s1, s2, origin));
+			}
+		}
+		else if (*s1 == '\0')
+			return (0);
+
+		else
+		{
+			s1++;
+			s2++;
+			return (asterisks(s1, s2, origin));
+		}
+	}
+}
