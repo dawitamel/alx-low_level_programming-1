66937b778055c7958f3209f69feaea7885aa0129
sofology <sofisintayehu19@gmail.com>
2021-09-23 22:16:55 +0300

    Header

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,19 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0C-more_malloc_free directory.
- */
-void _puts(char *str);
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
 
-#endif
+#endif /* MAIN_H */
