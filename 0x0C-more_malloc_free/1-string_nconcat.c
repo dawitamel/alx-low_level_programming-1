0bac64af5335ca663de64a016253f8d67bba778d
sofology <sofisintayehu19@gmail.com>
2021-09-23 22:03:54 +0300

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,49 @@
-#include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - concatenates 2 strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes to copy from second string
- * Return: pointer to new string
- */
-
+  * string_nconcat - concatenates two strings.
+  * @s1: pointer to first string.
+  * @s2: pointer to second string from which we'll take first n bytes.
+  * @n: n bytes of s2.
+  *
+  * Return: pointer to newly allocated space in memory.
+  * NULL if malloc fails.
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1 = 0, len2 = 0;
-	char *new_str;
+	char *nstr;
+	unsigned int len1, len2, size, i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (*(s1 + len1) != '\0')
+	len1 = 0;
+	while (s1[len1] != '\0')
 		len1++;
-	while (*(s2 + len2) != '\0')
+	len2 = 0;
+	while (s2[len2] != '\0')
 		len2++;
+
 	if (n >= len2)
 		n = len2;
-	new_str = malloc(sizeof(char) * (len1 + n + 1));
-	if (new_str == NULL)
+	size = len1 + n;
+
+	nstr = malloc((sizeof(char) * size) + 1);
+	if (nstr == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		new_str[i] = s1[i];
-	for (j = 0; j < n; i++, j++)
-		new_str[i] = s2[j];
-	new_str[i] = '\0';
-	return (new_str);
+	i = 0;
+	while (i < len1)
+	{
+		nstr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < size)
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	nstr[i] = '\0';
+	return (nstr);
 }
